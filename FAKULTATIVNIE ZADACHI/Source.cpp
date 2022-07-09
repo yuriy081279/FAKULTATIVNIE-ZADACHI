687649648c7753b2730895d8388921aad0eef3c5
Юрий Лейкехман <leikehman@yandex.ru>
2022-07-09 21:34:56 +0300

    Samaya poslednyaya versiya zadachi

--- FAKULTATIVNIE ZADACHI/Source.cpp
+++ FAKULTATIVNIE ZADACHI/Source.cpp
@@ -4,18 +4,18 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
-	const int STROKA = 15;
+	const int STROKA = 8;
 	const int STOLB = 6;
 	int arr[STROKA][STOLB];
 	int x = 0;
-	int y = STROKA - STOLB;
+
 	//Заполнение матрицы случайными числами
 	for (int i = 0; i < STROKA; i++)
 	{
 		for (int j = 0; j < STOLB; j++)
 		{
-			arr[i][j] = rand() % 6;				
-		}		
+			arr[i][j] = rand() % 7;
+		}
 	}
 	cout << endl;
 	//Вывод исходной матрицы на экран
@@ -26,7 +26,7 @@ void main()
 	{
 		for (int j = 0; j < STOLB; j++)
 		{
-			cout << arr[i][j]<<tab;
+			cout << arr[i][j] << tab;
 		}
 		cout << endl;
 	}
@@ -36,43 +36,38 @@ void main()
 	cout << endl;
 	for (int i = STROKA - 1; i >= 0; i--)
 	{
-		int j = STOLB - 1;
 		for (int j = STOLB - 1; j >= 0; j--)
 		{
-			//cout << "arr[i][j]= " << arr[i][j];
-			//cout << endl;
 			for (int c = 0; c <= i; c++)
 			{
 				for (int d = 0; d < STOLB; d++)
-				{					
-				    if (arr[i][j] == arr[c][d])
+				{
+					if (arr[i][j] == arr[c][d])
 					{
-					  ++x;
+						++x;
 					}
 				}
-			}			
+			}
+
 			for (int z = i; z < STROKA; z++)
 			{
-				if (y >= 0)
+				if (z == i)
 				{
-					for (int t = j; t < STOLB + y; t++)
-					{						
+					for (int t = j; t < STOLB; t++)
+					{
 						if (arr[i][j] == arr[z][t + 1] || arr[i][j] == arr[z + 1][t])
 						{
-							x = 0;							
+							x = 0;
 						}
-
 					}
 				}
-				if (y < 0)
+				if (z > i)
 				{
-					for (int t = j; t < STOLB - y; t++)
+					for (int t = 0; t < STOLB; t++)
 					{
-						//cout << "arr[z][t+1]= " << arr[z][t+1];
-						//cout << endl;
 						if (arr[i][j] == arr[z][t + 1] || arr[i][j] == arr[z + 1][t])
 						{
-						  x = 0;							
+							x = 0;
 						}
 					}
 				}
@@ -85,6 +80,6 @@ void main()
 				cout << endl;
 			}
 			x = 0;
-		}				
+		}
 	}
 }
