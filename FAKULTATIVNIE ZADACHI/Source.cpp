773ef12983fc42548bd4e6cc4bf0ebb0e631f39c
Юрий Лейкехман <leikehman@yandex.ru>
2022-08-05 13:00:30 +0300

    ostalnie zadachi

--- FAKULTATIVNIE ZADACHI/Source.cpp
+++ FAKULTATIVNIE ZADACHI/Source.cpp
@@ -1,14 +1,17 @@
 ﻿#include<iostream>
 using namespace std;
 #define tab "\t"
+//#define same_znacheniya
+//#define random_s_boolom // Плюс сортировка "в одном флаконе"
+#define random_bez_boola// Плюс сортировка "в одном флаконе"
+#ifdef same_znacheniya
 void main()
 {
 	setlocale(LC_ALL, "");
-	const int STROKA = 8;
+	const int STROKA = 7;
 	const int STOLB = 6;
 	int arr[STROKA][STOLB];
 	int x = 0;
-
 	//Заполнение матрицы случайными числами
 	for (int i = 0; i < STROKA; i++)
 	{
@@ -48,7 +51,6 @@ void main()
 					}
 				}
 			}
-
 			for (int z = i; z < STROKA; z++)
 			{
 				if (z == i)
@@ -83,3 +85,182 @@ void main()
 		}
 	}
 }
+#endif // same_znacheniya
+#ifdef random_bez_boola
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int STROKA = 6;
+	const int STOLB = 4;
+	int arr[STROKA][STOLB];
+	int x = 0;
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{			
+			arr[i][j] = rand() % (STROKA * STOLB);
+			for (int c = 0; c <= i; c++)
+			{
+				if (c == i)
+				{
+					for (int d = 0; d < j; d++)
+					{
+						if (arr[i][j] == arr[c][d])
+						{
+							x++;
+							//break;
+						}
+					}
+				}
+				if (c < i)
+				{
+					for (int d = 0; d < STOLB; d++)
+					{
+						if (arr[i][j] == arr[c][d])
+						{
+							x++;
+							//break;														
+						}
+					}
+				}
+				if (x > 0)
+				{
+					if (j > 0)
+					{
+						j--;												
+					}
+					if (j == 0)
+					{
+						i--;
+						j = STOLB;
+				    }
+				}
+			}			
+			x = 0;					
+		}
+	}
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+	cout << " Этот же массив в отсортированном виде: " << endl;
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			for (int c = 0; c < STROKA; c++)
+			{
+				for (int d = 0; d < STOLB; d++)
+				{
+					if (arr[i][j] < arr[c][d])
+					{
+						int min = arr[i][j];
+						arr[i][j] = arr[c][d];
+						arr[c][d] = min;
+					}
+				}
+			}
+		}
+	}
+	cout << endl;
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+
+}
+
+#endif // random_bez_boola
+#ifdef random_s_boolom
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int STROKA = 6;
+	const int STOLB = 4;
+	int arr[STROKA][STOLB];
+	bool unique;	
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			do
+			{
+				unique = true;
+				arr[i][j] = rand() % (STROKA * STOLB);
+				for (int c = 0; c <= i; c++)
+				{
+					if (c == i)
+					{
+						for (int d = 0; d < j; d++)
+						{
+							if (arr[i][j] == arr[c][d])
+							{
+								unique = false;
+								break;
+							}
+						}
+					}
+					if (c < i)
+					{
+						for (int d = 0; d < STOLB; d++)
+						{
+							if (arr[i][j] == arr[c][d])
+							{
+								unique = false;
+								break;
+							}
+						}
+					}
+				}
+			} while (!unique);
+		}
+	}
+	cout << " Массив заполненный уникальными значениями: " << endl;
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+	cout << " Этот же массив в отсортированном виде: " << endl;
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			for (int c = 0; c < STROKA; c++)
+			{
+				for (int d = 0; d < STOLB; d++)
+				{
+					if (arr[i][j] < arr[c][d])
+					{
+						int min = arr[i][j];
+						arr[i][j] = arr[c][d];
+						arr[c][d] = min;
+					}
+				}				
+			}			
+		}		
+	}
+	cout << endl;
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+}
+#endif // random_s_boolom
+
+
